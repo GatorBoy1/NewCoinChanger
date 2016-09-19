1e3bad3031e7cb274872a9ccff3d135a89fe0e58
Don Kingery <kingery1@suddenlink.net>
2016-09-19 18:54:35 -0400

    coin_change_includes_zeroes_succesful

--- test_C.rb
+++ test_C.rb
@@ -6,7 +6,7 @@ class TestCoinChanger< Minitest::Test
         cents_received = 0
         assert_equal({dollar_pc:0, fifty_cnt_pc:0, quarter:0, dime:0, nickel:0, penny:0}, coin_changer(cents_received))
     end
-
+ 
     def test_returns_8_dollars_and_fifty_cents
         cents_received = 850
         assert_equal({dollar_pc:8, fifty_cnt_pc:1, quarter:0, dime:0, nickel:0, penny:0},coin_changer(cents_received))
@@ -29,6 +29,14 @@ def test_returns_4_dollars_and_ninety_three_cents
         assert_equal({dollar_pc:4, fifty_cnt_pc:1, quarter:1, dime:1, nickel:1, penny:3},coin_changer(cents_received))
 
     end
+
+
+def test_returns_4_dollars_and_three_cents
+        cents_received = 403
+        assert_equal({dollar_pc:4, fifty_cnt_pc:0, quarter:0, dime:0, nickel:0, penny:3},coin_changer(cents_received))
+
+    end
+
 
 
 
